40a4582eeecf13b0d35ab7885d19ec5e04e46e71
tuck1s <steve.tuck@sparkpost.com>
2021-09-17 18:46:26 +0100

    tweak

--- sending-domains.sh
+++ sending-domains.sh
@@ -3,6 +3,6 @@ echo "Searching files for unique sending domains:"
 for i in consume-mail.log*
 do
     echo "$i:"
-    # fix up file lines to be the same length, extract column 8 (sending domain), dedup, and show only lines with email addrs
+    # fix up file lines to be the same length, extract column (sending domain), dedup, and show only lines with email addrs
     cat $i | src/csvfix.py |  grep "\.msg" | csvcut -c 7 | sort | uniq | grep "@"
 done
